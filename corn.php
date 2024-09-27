5de95f494c84f4891ab92c0b7604b5c2e17dff66
Mahi <98208009+hctilg@users.noreply.github.com>
2024-09-27 17:03:29 +0330

    Update corn.php

--- corn.php
+++ corn.php
@@ -7,8 +7,9 @@
  */
 
 $ch = curl_init();
-// Replace your site address.
-curl_setopt($ch, CURLOPT_URL, "https://example.com/CuteYourDay/sender.php");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, "https://example.com/CuteYourDay/sender.php");  // Replace your site address.
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+curl_setopt($ch, CURLOPT_TIMEOUT_MS, 99999000);
+curl_setopt($ch, CURLOPT_TIMEOUT, 99999);
 curl_exec($ch);
 curl_close($ch);
