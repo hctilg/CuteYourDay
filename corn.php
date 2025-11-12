59bddd048e5e1df93d90d05f19db783eab67aa28
Mahoor Nemati ✨ <98208009+hctilg@users.noreply.github.com>
2025-11-12 19:19:23 +0330

    Update corn.php

--- corn.php
+++ corn.php
@@ -6,10 +6,4 @@
  *               0      *      *      *        *
  */
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://example.com/CuteYourDay/sender.php");  // Replace your site address.
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
-curl_setopt($ch, CURLOPT_TIMEOUT_MS, 99999000);
-curl_setopt($ch, CURLOPT_TIMEOUT, 99999);
-curl_exec($ch);
-curl_close($ch);
+require_once "sender.php";
